808fbf405ec0e06a6ac353ef2da126b682b48594
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-11-06 16:21:02 +0100

    Add another ModExp implementation

--- ModExp.java
+++ ModExp.java
@@ -7,12 +7,14 @@ public class ModExp {
 
     private static final BigInteger ZERO = BigInteger.ZERO;
     private static final BigInteger ONE  = BigInteger.ONE;
+    private static final BigInteger TWO  = BigInteger.valueOf(2);
 
     /*
      * Performs modular exponentiation on the given number as
      * per Applied Cryptography.
      */
     public static BigInteger modPow(BigInteger base, BigInteger exp, BigInteger mod) {
+    // public static BigInteger modPow(BigInteger a, BigInteger b, BigInteger n) {
         BigInteger res = ONE;
 
         while (exp.compareTo(ZERO) > 0) {
@@ -24,6 +26,15 @@ public class ModExp {
             base = (base.multiply(base)).mod(mod); // (base * base) % mod
         }
 
-        return res .mod(mod);
+        return res.mod(mod);
+
+        // // Return a^b mod n
+        // if (b.equals(ZERO)) return ONE;
+
+        // BigInteger t = modPow(a, b.divide(TWO), n);
+        // BigInteger c = (t.multiply(t)).mod(n);
+
+        // if (b.mod(TWO).equals(ONE)) c = (c.multiply(a)).mod(n);
+        // return c;
     }
 }
