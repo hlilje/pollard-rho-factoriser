7c7d60501c40d5c49612e177daefede0c07c24ff
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-11-12 20:27:28 +0100

    Add initialisation method

--- QuadraticSieve.java
+++ QuadraticSieve.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 /**
  * Implementation of the quadratic sieve algorithm for factoring
@@ -6,12 +7,17 @@ import java.math.BigInteger;
  */
 public class QuadraticSieve {
 
-    private static final BigInteger ZERO  = BigInteger.ZERO;
-    private static final BigInteger ONE   = BigInteger.ONE;
-    private static final BigInteger TWO   = BigInteger.valueOf(2);
-    private static final BigInteger THREE = BigInteger.valueOf(3);
+    private static final BigInteger ZERO        = BigInteger.ZERO;
+    private static final BigInteger ONE         = BigInteger.ONE;
+    private static final BigInteger TWO         = BigInteger.valueOf(2);
+    private static final BigInteger THREE       = BigInteger.valueOf(3);
 
     private static int B; // Smoothness bound
+    private static int K;
+
+    private static ArrayList<BigInteger> primes = new ArrayList<BigInteger>();
+    private static ArrayList<BigInteger> roots  = new ArrayList<BigInteger>();
+
 
     /**
      * L function for quadratic sieve.
@@ -22,6 +28,36 @@ public class QuadraticSieve {
     }
 
     /**
+     * Finds a root +-a_i with (a_i)^2 cong n (mod p_i).
+     */
+    private static BigInteger findRoot(BigInteger n) {
+        return ONE;
+    }
+
+    /**
+     * Initialises the quadratic sieve algorithm.
+     */
+    private static void initialise(BigInteger n) {
+        // B could be tuned after taste instead
+        // L(n)^(1/2) rounded up
+        BigInteger B = BigInteger.valueOf((int) Math.ceil(Math.sqrt(L(n))));
+        primes.add(TWO); // p_1 = 2
+        roots.add(ONE); // a_1 = 1
+
+        // Find all odd primes <= B for which n/p == 1 holds and label them
+        for (BigInteger p = THREE; p.compareTo(B) <= 0; p = p.add(ONE)) {
+            if(MillerRabin.isProbablePrime(p) && (n.divide(p) == ONE)) {
+                primes.add(p);
+            }
+        }
+
+        K = primes.size();
+        for (BigInteger a = TWO; a.compareTo(BigInteger.valueOf(K)) <= 0; a = a.add(ONE)) {
+            roots.add(a);
+        }
+    }
+
+    /**
      * Finds one factor of the given number using the quadratic sieve
      * algorithm.
      */
@@ -29,17 +65,7 @@ public class QuadraticSieve {
         // Return if divisible by 2
         if (n.mod(TWO).equals(ZERO)) return TWO;
 
-        // B could be tuned after taste instead
-        // L(n)^(1/2) rounded up
-        BigInteger B = BigInteger.valueOf((int) Math.ceil(Math.sqrt(L(n))));
-        BigInteger p1, a;
-
-        // Find all odd primes <= B for which n/p == 1 holds
-        for (BigInteger p = THREE; p.compareTo(B) <= 9; p = p.add(ONE)) {
-            if(MillerRabin.isProbablePrime(p) && (n.divide(p) == ONE)) {
-                // TODO
-            }
-        }
+        initialise(n);
 
         return ONE;
     }
